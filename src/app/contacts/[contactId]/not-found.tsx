75fdee191b3cc81357b300fe2b64c465ec8f8181
Aleksander Melnik <alexander.melnik@pandadoc.com>
2024-12-17 21:26:26 +0100

    routes planned

---
+++ src/app/contacts/[contactId]/not-found.tsx
@@ -0,0 +1,3 @@
+export default function ContactNotFoundPage() {
+  return <div>Contact was not found</div>;
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function PageNotFound() {
+  return <div>Page was not found</div>;
+}
